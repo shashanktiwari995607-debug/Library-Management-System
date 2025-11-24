e6449eaa8baeb6dd60bdd73c348f201d40fa31e9
shashanktiwari995607-debug <shashanktiwari995607@gmail.com>
2025-11-24 23:20:08 +0530

    Add TransactionRecord model class

---
+++ src/main/java/com/lms/model/TransactionRecord.java
@@ -0,0 +1,81 @@
+package com.lms.model;
+
+import java.sql.Date;
+
+public class TransactionRecord {
+    private int txnId;
+    private int bookId;
+    private int memberId;
+    private Date borrowDate;
+    private Date dueDate;
+    private Date returnDate;
+    private String status; // BORROWED / RETURNED / OVERDUE
+
+    public TransactionRecord() {}
+
+    public TransactionRecord(int txnId, int bookId, int memberId, Date borrowDate, Date dueDate, Date returnDate, String status) {
+        this.txnId = txnId;
+        this.bookId = bookId;
+        this.memberId = memberId;
+        this.borrowDate = borrowDate;
+        this.dueDate = dueDate;
+        this.returnDate = returnDate;
+        this.status = status;
+    }
+
+    public int getTxnId() {
+        return txnId;
+    }
+
+    public void setTxnId(int txnId) {
+        this.txnId = txnId;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
+    }
+
+    public Date getBorrowDate() {
+        return borrowDate;
+    }
+
+    public void setBorrowDate(Date borrowDate) {
+        this.borrowDate = borrowDate;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
