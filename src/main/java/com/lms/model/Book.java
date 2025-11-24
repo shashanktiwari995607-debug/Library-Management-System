142abb6e6500fd67c50f84501e2ce015885dd3d6
shashanktiwari995607-debug <shashanktiwari995607@gmail.com>
2025-11-24 23:10:25 +0530

    Add Book model class with properties and methods

---
+++ src/main/java/com/lms/model/Book.java
@@ -0,0 +1,69 @@
+package com.lms.model;
+
+public class Book {
+    private int bookId;
+    private String title;
+    private String author;
+    private String isbn;
+    private int totalCopies;
+    private int availableCopies;
+
+    public Book() {}
+
+    public Book(int bookId, String title, String author, String isbn, int totalCopies, int availableCopies) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.totalCopies = totalCopies;
+        this.availableCopies = availableCopies;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getTotalCopies() {
+        return totalCopies;
+    }
+
+    public void setTotalCopies(int totalCopies) {
+        this.totalCopies = totalCopies;
+    }
+
+    public int getAvailableCopies() {
+        return availableCopies;
+    }
+
+    public void setAvailableCopies(int availableCopies) {
+        this.availableCopies = availableCopies;
+    }
+}
